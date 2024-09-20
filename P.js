5d9e4d9326f821acf439d1902ce6d6bc2ccba74a
Goxome <56850970+Goxome@users.noreply.github.com>
2024-09-20 22:41:29 +0530

    Update P.js

--- P.js
+++ P.js
@@ -5,7 +5,7 @@
   "media":"https://tmpfiles.org/dl/13099341/https___in.pinterest..._sfo1_18.mp4",
   "RemoteCode":101,
   "RemoteTitle":"Find",
-  "RemoteMessage":"1. adaptation Android 11+ \n2. New Stylish Crosshair\n3. No Notch Device Displaying issue fixed.",
+  "RemoteMessage":"1. adaptatiohn Android 11+ \n2. New Stylish Crosshair\n3. No Notch Device Displaying issue fixed.",
   "btn":"Ok",
   "btnUrl":"https://github.com/Goxome/FPS-Crosshair/releases/download/2.0.0/Release_v2.0-Beta.apk",
   "btn2":"Cancel",
